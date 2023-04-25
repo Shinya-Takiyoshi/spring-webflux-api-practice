97b0907d31b88fe3375fc7b34ac777cd19fe3fb7
Shinya-Takiyoshi <garo.savior@gmail.com>
2023-04-25 15:39:49 +0900

    api追加

--- src/main/java/com/example/webfluxapi/controller/HelloController.java
+++ src/main/java/com/example/webfluxapi/controller/HelloController.java
@@ -2,7 +2,18 @@ package com.example.webfluxapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
 
 @RestController
 public class HelloController {
@@ -11,9 +22,31 @@ public class HelloController {
     public Mono<String> getJust() {
         return Mono.just("Mono-just");
     }
-    /*@GetMapping("/monoZip")
-    public Mono<String> getString() {
-        return Mono.zip("Mono-zip");
-    }*/
+    @GetMapping("/monoZip")
+    public Mono<String> getZip() {
+        Mono<String> mono1 = Mono.just("Mono1");
+        Mono<String> mono2 = Mono.just("Mono2");
+        return Mono.zip(mono1,mono2).map(
+                tuple-> tuple.getT1()+ tuple.getT2());
+    }
+    @GetMapping("/FluxJust")
+    public Flux<String> getFluxJust() {
+        return Flux.just("Flux-just");
+    }
+
+    @GetMapping("/FluxZip")
+    public Flux<String> getFluxZip() {
+        Flux<String> flux1 = Flux.just("flux1");
+        Flux<String> flux2 = Flux.just("flux2");
+        return Flux.zip(flux1,flux2).map(
+                tuple-> tuple.getT1()+ tuple.getT2());
+    }
+
+    @GetMapping("/FluxInterval")
+    public Flux<String> getFluxInterval() {
+        return Flux.interval(Duration.ofMillis(300))
+                .map(i -> i +" "+ LocalDateTime.now())
+                .take(10);
+    }
 
 }
